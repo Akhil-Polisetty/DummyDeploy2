9c3f35c9368bfc25029e29ef1b379c7f47bca634
Akhil-Polisetty <polisettiakhi72073@gmail.com>
2025-05-11 21:37:21 +0530

    referal changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -40,7 +40,7 @@ router.post("/register", async (req, res) => {
       username,
       password: hashedPassword,
       wallet,
-      referralCode,
+      referralCode:referralCode,
       referrer: referrerCode || null,
       balance: 1000,
       hasDeposited: false,
